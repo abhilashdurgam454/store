81e57be1d19a148406a0a7a4729cc38f377ec35a
abhilashdurgam454 <durgamabhilash15@gmail.com>
2024-02-19 03:45:52 -0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        setError(false);
+        const response = await axios.get('https://fakestoreapi.com/products');
+        setProducts(response.data);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+        setError(true);
+        console.log("Error:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>       
+       <h1 className='text-center font-bold text-3xl'>This is a React app</h1>
+       {loading && <h1>Loading...</h1>}
+       {error && <h1>Something went wrong</h1>}
+       
+       <div className="flex flex-wrap justify-center">
+        {products.map((product) => (
+          <div
+            key={product.id}
+            className="m-4 w-48 h-58 border border-gray-400 p-4"
+          >
+            <img
+              src={product.image}
+              alt={product.title}
+              className="w-full h-82 object-contain mb-2"
+            />
+            <div className="text-sm truncate">{product.title}</div>
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
